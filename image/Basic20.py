80a989925f6ec24005c6250668afd793256a88c7
natanonkhuanta <101494144+Natanonkhuanata@users.noreply.github.com>
2024-06-20 14:29:21 +0700

    Update Basic20.py

---
+++ image/Basic20.py
@@ -0,0 +1,21 @@
+#ตรวจจับวัตถุกลุ่มสี
+
+import cv2
+import numpy
+
+while True:
+    img = cv2.imread("image/ball2d01.jpg")
+    img = cv2.resize(img,(400,400))
+
+    # มีภาพสีเเขียวอยู่เท่าไหร่
+    upper = numpy.array([124,255,133])
+    lower = numpy.array([5,111,10])
+
+    mask = cv2.inRange(img,lower,upper)
+    if cv2.waitKey(0) & 0xFF == ord('e'):
+        break
+
+    cv2.imshow("Original",img)
+    cv2.imshow("Mask",mask)
+
+cv2.destroyAllWindows()
