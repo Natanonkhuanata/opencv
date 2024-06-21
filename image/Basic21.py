1c2e893a3ce9959acdf4b84d15c61b4581d6483e
natanonkhuanta <101494144+Natanonkhuanata@users.noreply.github.com>
2024-06-21 08:13:42 +0700

    Update Basic21.py

--- image/Basic21.py
+++ image/Basic21.py
@@ -3,8 +3,6 @@
 import cv2
 
 #อ่านภาพ
-img = cv2.imread("image/Love.jpg")
-imgresize = cv2.resize(img,(600,600))
 img = cv2.imread("image/NINJA.jpg")
 imgresize = cv2.resize(img,(600,700))
 face_cascade = cv2.CascadeClassifier("image/Detect/haarcascade_frontalface_default.xml") #การอ้านไฟล์สำหรับ cassification
